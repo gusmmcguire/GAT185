d8d241a0c8b9b0ddcf0956832e7f9ad68f8e7e5a
Gus <gusmcg1856@gmail.com>
2022-01-19 11:00:28 -0700

    day 6

---
+++ Assets/SpaceShooter/Scripts/Health.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] GameObject deathPrefab;
+    [SerializeField] bool destroyOnDeath = true;
+    [SerializeField] float maxHealth = 100;
+
+    public float health { get; set; }
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            if (deathPrefab)
+            {
+                Instantiate(deathPrefab, transform.position, transform.rotation);
+            }
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
